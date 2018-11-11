a2a0100ea5a2443cc11e43b3b524f2ad523c022d
ssk13 <ssk13@ufl.edu>
2018-11-10 23:16:45 -0800

    fixing nav

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -22,14 +22,14 @@ class NavBar extends Component {
     render() {
         return (
             <ul className="header">
-                <li className={this.getClassName('developer')}>
-                    <NavLink to="/developer" onClick={() => {this.setState({activePage: 'developer'})}}>Developer</NavLink>
+                <li>
+                    <NavLink className={this.getClassName('developer')} to="/developer" onClick={() => {this.setState({activePage: 'developer'})}}>Developer</NavLink>
                 </li>
-                <li className={this.getClassName('musician')}>
-                    <NavLink to="/musician" onClick={() => {this.setState({activePage: 'musician'})}}>Musician</NavLink>
+                <li>
+                    <NavLink className={this.getClassName('musician')} to="/musician" onClick={() => {this.setState({activePage: 'musician'})}}>Musician</NavLink>
                 </li>
-                <li className={this.getClassName('photographer')}>
-                    <NavLink to="/photographer" onClick={() => {this.setState({activePage: 'photographer'})}}>Photographer</NavLink>
+                <li>
+                    <NavLink className={this.getClassName('photographer')} to="/photographer" onClick={() => {this.setState({activePage: 'photographer'})}}>Photographer</NavLink>
                 </li>
             </ul>
         );
