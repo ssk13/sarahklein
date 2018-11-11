eebdf6f661d7d7c8ed9ee474d7b251dc3dc13c01
ssk13 <ssk13@ufl.edu>
2018-11-10 20:54:07 -0800

    more style

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './style/navBar.css';
+
 class NavBar extends Component {
     constructor(props) {
         super(props);
